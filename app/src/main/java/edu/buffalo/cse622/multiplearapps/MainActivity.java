11747458bed2aaeb17bf28335a383fab754c2a40
Madhusudan Saha <msaha@buffalo.edu>
2019-11-03 21:09:21 -0500

    Added file chooser for apk loading.

--- app/src/main/java/edu/buffalo/cse622/multiplearapps/MainActivity.java
+++ app/src/main/java/edu/buffalo/cse622/multiplearapps/MainActivity.java
@@ -12,15 +12,14 @@ import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.OpenableColumns;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.ar.core.Frame;
 import com.google.ar.sceneform.AnchorNode;
@@ -30,8 +29,10 @@ import com.google.ar.sceneform.ux.ArFragment;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -39,21 +40,14 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-import dalvik.system.DexClassLoader;
 import dalvik.system.PathClassLoader;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context = null;
-    Map<Class<Object>, Object> pluginMap = new HashMap();
+    // Map with {Key:Value} pair as {PluginName:HashMap of class instances}. Inner map with {Key:Value} pair as {ClassName:Object array of size 2: [Class, Class instance]}
+    Map<String, HashMap<String, Object[]>> pluginMap = new HashMap();
     private ArFragment arFragment;
-
-    File targetApk = null;
-    Resources dynamicResources = null;
-    PathClassLoader loader = null;
-
-    Class<?> dynamicClass = null;
-    Object dynamicInstance = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,71 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mainToolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(mainToolbar);
+        context = this;
 
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_fragment);
 
         // Add a frame update listener to the scene to control the state of the buttons.
         arFragment.getArSceneView().getScene().addOnUpdateListener(this::onFrameUpdate);
 
-        context = this;
-        /*
-        ViewRenderable.builder()
-                .setView(this, R.layout.text_view)
-                .build()
-                .thenAccept(
-                        (renderable) -> {
-                            textRenderable = renderable;
-                        });
-        */
-        targetApk = loadApk();
-        loadResources(targetApk);
-
-        try {
-            // Get the resources
-            int someStringId = dynamicResources.getIdentifier(
-                    "someString", "string", "edu.buffalo.cse622.pottedplantplugin");
-            String someString = dynamicResources.getString(someStringId);
-
-            Class<?> rString = Class.forName("edu.buffalo.cse622.pottedplantplugin.R$string", true, loader);
-            int anotherStringId = rString.getField("anotherString").getInt(null);
-            String anotherString = dynamicResources.getString(anotherStringId);
-
-            Toast.makeText(this, someString, Toast.LENGTH_LONG).show();
-            Toast.makeText(this, anotherString, Toast.LENGTH_LONG).show();
-
-            /*
-            int layoutId = dynamicResources.getIdentifier("text_view", "layout","edu.buffalo.cse622.pottedplantplugin");
-            XmlResourceParser textViewXml = dynamicResources.getLayout(layoutId);
-            View view = getLayoutInflater().inflate(textViewXml, null);
-
-            ViewRenderable.builder()
-                    .setView(this, view)
-                    .build()
-                    .thenAccept(
-                            (renderable) -> {
-                                textRenderable = renderable;
-                            });
-            */
-
-            PathClassLoader loader = new PathClassLoader(
-                    targetApk.getAbsolutePath(), getClassLoader());
-            dynamicClass = loader.loadClass("edu.buffalo.cse622.pottedplantplugin.FrameOperations");
-            Constructor<?> ctor = dynamicClass.getConstructor(Resources.class, Context.class);
-            dynamicInstance = ctor.newInstance(dynamicResources, context);
-
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
+        pluginMap = new HashMap<>();
     }
 
     private void requestAppPermissions() {
@@ -170,13 +107,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.import_app) {
-            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
             intent.setType("*/*");
 
-            startActivityForResult(Intent.createChooser(intent, "Choose jar to import"), 102);
-
-            //return true;
+            startActivityForResult(Intent.createChooser(intent, "Choose apk to import"), 102);
         }
 
         return super.onOptionsItemSelected(item);
@@ -188,96 +123,92 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 102 && resultCode == RESULT_OK) {
-            Uri selectedfile = data.getData(); //The uri with the location of the file
+            Uri selectedFile = data.getData(); //The uri with the location of the file
 
-            String path = Environment.getExternalStorageDirectory() + "/" + selectedfile.getPath().split(":")[1];
-            Log.d("onActivityResult", path);
 
-            Class<Object> classToLoad = getAppInstance(path);
-        }
-    }
+            try {
+                // Get file name and file InputStream
+                Cursor returnCursor =
+                        getContentResolver().query(selectedFile, null, null, null, null);
+                returnCursor.moveToFirst();
+                int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                String pluginName = returnCursor.getString(nameIndex);
+                InputStream apkInputStream = getContentResolver().openInputStream(selectedFile);
 
-    Class<Object> getAppInstance(String jarPath) {
-        Class<Object> classToLoad = null;
+                File targetApk = loadApk(apkInputStream);
+                Resources dynamicResources = loadResources(targetApk);
 
-        try {
-            File tmpDir = getDir("dex", 0);
-            DexClassLoader classloader = new DexClassLoader(jarPath, tmpDir.getAbsolutePath(), null, this.getClass().getClassLoader());
-            classToLoad = (Class<Object>) classloader.loadClass("edu.buffalo.cse622.pottedplant.FrameOperations");
-            Object instance = classToLoad.newInstance();
+                PathClassLoader loader = new PathClassLoader(
+                        targetApk.getAbsolutePath(), getClassLoader());
+                Class<?> dynamicClass = loader.loadClass("edu.buffalo.cse622.pottedplantplugin.FrameOperations");
+                Constructor<?> ctor = dynamicClass.getConstructor(Resources.class, Context.class);
+                Object dynamicInstance = ctor.newInstance(dynamicResources, context);
 
-            if (!pluginMap.containsKey(classToLoad)) {
-                pluginMap.put(classToLoad, instance);
+                // Add the class instances to pluginMap
+                if (pluginMap.containsKey(pluginName)) {
+                    HashMap<String, Object[]> instanceMap = pluginMap.get(pluginName);
+                    instanceMap.put("FrameOperations", new Object[]{dynamicClass, dynamicInstance});
+                    pluginMap.put(pluginName, instanceMap);
+                } else {
+                    HashMap<String, Object[]> instanceMap = new HashMap<>();
+                    instanceMap.put("FrameOperations", new Object[]{dynamicClass, dynamicInstance});
+                    pluginMap.put(pluginName, instanceMap);
+                }
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-        return classToLoad;
     }
 
     private void onFrameUpdate(FrameTime unusedframeTime) {
 
         Frame frame = arFragment.getArSceneView().getArFrame();
 
-        /*
-        FrameOperations frameOperations = new FrameOperations();
-        Node node = frameOperations.processFrame(frame, textRenderable);
-        if (node != null) {
-            node.getParent().setParent(arFragment.getArSceneView().getScene());
-        }
-        */
-        try {
-            Class[] classArguments = new Class[1];
-            classArguments[0] = Frame.class;
-
-            Method processFrame = dynamicClass.getDeclaredMethod("processFrame", classArguments);
-            processFrame.setAccessible(true);  // To invoke protected or private methods
-            AnchorNode anchorNode = (AnchorNode) processFrame.invoke(dynamicInstance, frame);
-            if (anchorNode != null) {
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
-            }
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        /*
-        for (Map.Entry<Class<Object>, Object> entry : pluginMap.entrySet()) {
-            Class<Object> classToLoad = entry.getKey();
-            Object instance = entry.getValue();
+        // Invoke processFrame on all plugins.
+        for (Map.Entry<String, HashMap<String, Object[]>> entry : pluginMap.entrySet()) {
+            Object[] instanceClassAndObject = entry.getValue().get("FrameOperations");
+            Class<?> dynamicClass = (Class<?>) instanceClassAndObject[0];
+            Object dynamicInstance = instanceClassAndObject[1];
 
             try {
-                Class[] classArguments = new Class[2];
-                classArguments[0] = Frame.class;
-                classArguments[1] = ViewRenderable.class;
-                System.out.println(classToLoad.getDeclaredMethods()[0]);
-                Method processFrame = classToLoad.getDeclaredMethod("processFrame", classArguments);
+                Method processFrame = dynamicClass.getDeclaredMethod("processFrame", Frame.class);
                 processFrame.setAccessible(true);  // To invoke protected or private methods
-                Node node = (Node) processFrame.invoke(instance, frame, textRenderable);
-                if (node != null) {
-                    node.getParent().setParent(arFragment.getArSceneView().getScene());
+                AnchorNode anchorNode = (AnchorNode) processFrame.invoke(dynamicInstance, frame);
+                if (anchorNode != null) {
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
                 }
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
             }
         }
-        */
+
     }
 
-    File loadApk() {
+    /**
+     * This method loads the apk using the file's InputStream.
+     *
+     * @param apkInputStream
+     * @return
+     */
+    File loadApk(InputStream apkInputStream) {
         File targetApk = new File(getDir("dex", Context.MODE_PRIVATE), "app.apk");
-        // copy APK from assets to private directory
-        // remove this condition in order to keep dynamic APK fresh
+
         if (!targetApk.exists() || !targetApk.isFile()) {
-            try (BufferedInputStream bis = new BufferedInputStream(
-                    getAssets().open("app-debug.apk"));
+            try (BufferedInputStream bis = new BufferedInputStream(apkInputStream);
                  OutputStream dexWriter = new BufferedOutputStream(
                          new FileOutputStream(targetApk))) {
 
@@ -294,8 +225,16 @@ public class MainActivity extends AppCompatActivity {
         return targetApk;
     }
 
-    void loadResources(File targetApk) {
-        loader = new PathClassLoader(
+    /**
+     * Initialize the dynamicResources object that is used for loading all the plugin resources.
+     *
+     * @param targetApk
+     * @return
+     */
+    Resources loadResources(File targetApk) {
+        Resources dynamicResources = null;
+
+        PathClassLoader loader = new PathClassLoader(
                 targetApk.getAbsolutePath(), getClassLoader());
         try {
             AssetManager assets = AssetManager.class.newInstance();
@@ -330,6 +269,8 @@ public class MainActivity extends AppCompatActivity {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        return dynamicResources;
     }
 
 }
